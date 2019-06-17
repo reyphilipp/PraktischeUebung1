aad7a19194771ecaf51b9bf1515bc07e8cf4b5a0
fabian jerg <fabian.jerg@bbbaden.ch>
2019-06-17 21:09:45 +0200

    .gitignore fix

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\99_projekte\Python\vps\PraktischeUebung1_philip\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
